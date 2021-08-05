ac81a7b735bba1e07a663fc78ef0009ffbea320e
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-05 09:47:21 +0900

    Update Make_model(5).py

--- MMDetection/Make_model(5).py
+++ MMDetection/Make_model(5).py
@@ -3,6 +3,10 @@ config_file = '/content/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_c
 checkpoint_file = '/content/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
 # config 파일과 pretrained 모델을 기반으로 Detector 모델을 생성. 
+# mmdetection은 상대 경로를 인자로 주면 무조건 mmdetection 디렉토리를 기준으로 함. 
+%cd mmdetection
+
 from mmdet.apis import init_detector, inference_detector
 
-model = init_detector(config_file, checkpoint_file, device='cuda:0')
+# init_detector() 인자로 config와 checkpoint를 입력함. 
+model = init_detector(config='configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py', checkpoint='checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth')
