cd2ed09bcd029a054a825c73e01c9f15cdb905e5
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-05 09:35:29 +0900

    Create install_mmcv.py

---
+++ MMDetection/install_mmcv.py
@@ -0,0 +1,7 @@
+import torch
+print(torch.__version__)
+
+!pip install mmcv-full
+
+#MMDetection is made based on PyTorch. Therefore Should import torch
+#install mmcv-full to use MMDetection. This will take about 10 minutes
