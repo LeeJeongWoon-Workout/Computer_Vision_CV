c9149c7aaf42df76fb5af4fd144f21ed783b9297
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-05 09:40:58 +0900

    Create Pretrained_Model_Download_From_MMDetection_github.py

---
+++ MMDetection/Pretrained_Model_Download_From_MMDetection_github.py
@@ -0,0 +1,7 @@
+# pretrained weight 모델을 다운로드 받기 위해서 mmdetection/checkpoints 디렉토리를 만듬. 
+!cd mmdetection; mkdir checkpoints
+
+!wget -O /content/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
+  
+  
+ #Link Address:https://github.com/open-mmlab/mmdetection/tree/master/configs/faster_rcnn
