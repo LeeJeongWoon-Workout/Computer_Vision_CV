f04acf1b24043c825c668db0d7005198b6444ba6
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-05 10:58:54 +0900

    Update install_mmcv(1).py

--- MMDetection/install_mmcv(1).py
+++ MMDetection/install_mmcv(1).py
@@ -3,5 +3,5 @@ print(torch.__version__)
 
 !pip install mmcv-full
 
-#MMDetection is made based on PyTorch. Therefore Should import torch
-#install mmcv-full to use MMDetection. This will take about 10 minutes
+#우리가 사용할 MMDetection의 모델은 PyTorch 기반으로 짜여져 있기 때문에 Pytorch를 다운받을 겁니다.
+#또한 MMDetection을 사용하기 위해서는 mmcv를 다운받아야 하는데 이 mmcv가 용량이 커 약 10분의 시간이 소요됩니다.
