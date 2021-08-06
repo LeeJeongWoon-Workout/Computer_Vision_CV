717bae21bd955d4b32e7d29065050a1d75c09d0f
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-07 08:27:56 +0900

    Update Setting.py

--- Custom_dataset_Kitty/Setting.py
+++ Custom_dataset_Kitty/Setting.py
@@ -3,6 +3,8 @@
 # mmdetection 설치 
 !git clone https://github.com/open-mmlab/mmdetection.git
 !cd mmdetection; python setup.py install
+#내 드라이브에 mmdetection directory를 생성한 후 가져온 깃헙 정보를 python setup 으로 install 한다.
+
 
 # 아래를 수행하기 전에 kernel을 restart 해야 함. 
 from mmdet.apis import init_detector, inference_detector
