dd6342443033062c9ccf3c7a5965d880730bc353
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-07 09:24:26 +0900

    Create train.py

---
+++ Custom_dataset_Kitty/train.py
@@ -0,0 +1,28 @@
+from mmdet.datasets import build_dataset
+from mmdet.models import build_detector
+from mmdet.apis import train_detector
+
+# train용 Dataset 생성. 
+datasets = [build_dataset(cfg.data.train)]
+
+model = build_detector(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
+model.CLASSES = datasets[0].CLASSES
+
+# 주의, config에 pretrained 모델 지정이 상대 경로로 설정됨 cfg.load_from = 'checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+# 아래와 같이 %cd mmdetection 지정 필요. 
+ 
+%cd mmdetection 
+
+mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+# epochs는 config의 runner 파라미터로 지정됨. 기본 12회 
+train_detector(model, datasets, cfg, distributed=False, validate=True)
+
+from mmdet.apis import inference_detector, init_detector, show_result_pyplot
+
+# BGR Image 사용 
+img = cv2.imread('/content/kitti_tiny/training/image_2/000068.jpeg')
+
+model.cfg = cfg
+
+result = inference_detector(model, img)
+show_result_pyplot(model, img, result)
