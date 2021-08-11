cdc0b68ad2ccf55b87afe97fdf84f423c87dbf72
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-11 13:41:29 +0900

    Create train.py

---
+++ MMDetection_CoCo/train.py
@@ -0,0 +1,11 @@
+from mmdet.datasets import build_dataset
+from mmdet.models import build_detector
+from mmdet.apis import train_detector
+
+# train용 Dataset 생성. 
+datasets = [build_dataset(cfg.data.train)]
+
+model = build_detector(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
+model.CLASSES = datasets[0].CLASSES
+print(model.CLASSES)
+train_detector(model, datasets, cfg, distributed=False, validate=True)
