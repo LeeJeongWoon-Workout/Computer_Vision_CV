e6f87921a30ea04845092785ffab960ef9707060
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-13 10:39:29 +0900

    Create config_model_download.py

---
+++ Aerial Maritime Drone Dataset/config_model_download.py
@@ -0,0 +1,56 @@
+config_file = '/content/mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+checkpoint_file = '/content/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+!cd /content/mmdetection; mkdir checkpoints
+!wget -O /content/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
+
+from mmcv import Config
+
+cfg = Config.fromfile(config_file)
+print(cfg.pretty_text)
+from mmdet.apis import set_random_seed
+
+# dataset에 대한 환경 파라미터 수정. 
+cfg.dataset_type = 'PKLotDataset'
+cfg.data_root = '/content/BCCD_Dataset/BCCD/'
+
+# train, val, test dataset에 대한 type, data_root, ann_file, img_prefix 환경 파라미터 수정. 
+cfg.data.train.type = 'PKLotDataset'
+cfg.data.train.data_root = '/content/data/'
+cfg.data.train.ann_file = 'train/_annotations.coco.json'
+cfg.data.train.img_prefix = 'train'
+
+cfg.data.val.type = 'PKLotDataset'
+cfg.data.val.data_root = '/content/data/'
+cfg.data.val.ann_file = 'valid/_annotations.coco.json'
+cfg.data.val.img_prefix = 'valid'
+
+cfg.data.test.type = 'PKLotDataset'
+cfg.data.test.data_root = '/content/data/'
+cfg.data.test.ann_file = 'test/_annotations.coco.json'
+cfg.data.test.img_prefix = 'test'
+
+# class의 갯수 수정. 
+cfg.model.roi_head.bbox_head.num_classes = 6
+# pretrained 모델
+cfg.load_from = '/content/mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+
+# 학습 weight 파일로 로그를 저장하기 위한 디렉토리 설정. 
+cfg.work_dir = './tutorial_exps'
+
+# 학습율 변경 환경 파라미터 설정. 
+cfg.optimizer.lr = 0.02 / 8
+cfg.lr_config.warmup = None
+cfg.log_config.interval = 10
+
+# CocoDataset의 경우 metric을 bbox로 설정해야 함.(mAP아님. bbox로 설정하면 mAP를 iou threshold를 0.5 ~ 0.95까지 변경하면서 측정)
+cfg.evaluation.metric = 'bbox'
+cfg.evaluation.interval = 12
+cfg.checkpoint_config.interval = 12
+
+# 두번 config를 로드하면 lr_config의 policy가 사라지는 오류로 인하여 설정. 
+cfg.lr_config.policy='step'
+# Set seed thus the results are more reproducible
+cfg.seed = 0
+set_random_seed(0, deterministic=False)
+cfg.gpu_ids = range(1)
