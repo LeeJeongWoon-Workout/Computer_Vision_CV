85db2f634b937b2c987e9c1798fe60ae1684a662
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-13 17:33:57 +0900

    Create download_data.py

---
+++ Wild_file/Setting/download_data.py
@@ -0,0 +1,9 @@
+!mkdir /content/data
+!mkdir /content/data/annotations
+!mkdir /content/data/train
+!mkdir /content/data/test
+!mkdir /content/data/valid
+
+!cd /content/data/train && unzip train.zip 
+!cd /content/data/test && unzip test.zip 
+!cd /content/data/valid && unzip valid.zip 
