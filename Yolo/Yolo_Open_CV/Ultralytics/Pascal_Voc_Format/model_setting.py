88676f0fd9506cc42003e9e5e91c8fd969cb5d39
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-19 18:47:24 +0900

    Create model_setting.py

---
+++ Yolo/Yolo_Open_CV/Ultralytics/Pascal_Voc_Format/model_setting.py
@@ -0,0 +1,5 @@
+#ultralytics package installing from github open source
+#download in yolov3 directory
+
+!git clone https://github.com/ultralytics/yolov3
+!cd yolov3;pip install -qr requirements.txt
