fd4aa47c7018c7a897f89f6406e42a825321675e
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-19 18:54:55 +0900

    Create yolo_format.py

---
+++ Yolo/Yolo_Open_CV/Ultralytics/Pascal_Voc_Format/yolo_format.py
@@ -0,0 +1,19 @@
+import shutil
+
+def make_yolo_anno_file(df, tgt_images_dir, tgt_labels_dir):
+  for index, row in df.iterrows():
+    src_image_path = row['img_filepath']
+    src_label_path = row['anno_filepath']
+    # 이미지 1개당 단 1개의 오브젝트만 존재하므로 class_name을 object_name으로 설정.  
+    object_name = row['class_name']
+    # yolo format으로 annotation할 txt 파일의 절대 경로명을 지정. 
+    target_label_path = tgt_labels_dir + row['img_name']+'.txt'
+    # image의 경우 target images 디렉토리로 단순 copy
+    shutil.copy(src_image_path, tgt_images_dir)
+    # annotation의 경우 xml 파일을 target labels 디렉토리에 Ultralytics Yolo format으로 변환하여  만듬
+    xml_to_txt(src_label_path, target_label_path, object_name)
+
+# train용 images와 labels annotation 생성. 
+make_yolo_anno_file(train_df, '/content/ox_pet/images/train/', '/content/ox_pet/labels/train/')
+# val용 images와 labels annotation 생성. 
+make_yolo_anno_file(val_df, '/content/ox_pet/images/val/', '/content/ox_pet/labels/val/')
