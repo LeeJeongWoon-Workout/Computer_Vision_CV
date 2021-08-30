3710fd648ead7179c53b2a7a3b98b92fee0dc6e9
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-08-31 08:39:13 +0900

    Create video_detect_func.py

---
+++ Segmentation/train_pascal_voc/video_detect_func.py
@@ -0,0 +1,32 @@
+import time
+
+def do_detected_video(model, input_path, output_path, score_threshold, do_print=True):
+    
+    cap = cv2.VideoCapture(input_path)
+
+    codec = cv2.VideoWriter_fourcc(*'XVID')
+
+    vid_size = (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    vid_fps = cap.get(cv2.CAP_PROP_FPS)
+
+    vid_writer = cv2.VideoWriter(output_path, codec, vid_fps, vid_size) 
+
+    frame_cnt = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print('총 Frame 갯수:', frame_cnt)
+    btime = time.time()
+    while True:
+        hasFrame, img_frame = cap.read()
+        if not hasFrame:
+            print('더 이상 처리할 frame이 없습니다.')
+            break
+        stime = time.time()
+        img_frame = get_detected_img(model, img_frame,  score_threshold=score_threshold,is_print=False)
+        if do_print:
+          print('frame별 detection 수행 시간:', round(time.time() - stime, 4))
+        vid_writer.write(img_frame)
+    # end of while loop
+
+    vid_writer.release()
+    cap.release()
+
+    print('최종 detection 완료 수행 시간:', round(time.time() - btime, 4))
